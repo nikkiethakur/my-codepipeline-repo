3c247f833a066a264b05fc339506b27b18d13151
nikkiethakur <nikkiethakur74@gmail.com>
2025-03-21 17:43:08 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Detect if running on Windows Git Bash
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
-    echo "Detected Windows. Running commands without sudo."
-    apt install awscli -y  # Remove sudo
-else
-    echo "Running on Linux/macOS. Using sudo."
-    sudo apt update -y
-    sudo apt install awscli -y
-fi
+sudo apt update -y
+sudo apt install -y python3-pip
